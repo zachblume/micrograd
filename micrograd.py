59a585271d05c87ccfd1651438e019dc32392110
Zach Blume <zachblume@gmail.com>
2024-12-08 22:25:15 -0500

    first commit

---
+++ micrograd.py
@@ -0,0 +1,14 @@
+class Value:
+    """Wraps a Number as a node in a computation graph."""
+
+    def __init__(self):
+        pass
+
+    def __add__(self,b):
+
+class Neuron:
+    """"""
+
+class MultiLayerPerception:
+    """A very simple network graph of neurons"""
+
